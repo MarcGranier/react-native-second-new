8395cfd5d4bb9a7b70d963698f7d39db57b6d357
Marc Granier <marcgranier969@gmail.com>
2022-09-12 18:10:49 -0400

    Local Notifications

--- screens/ReservationScreen.js
+++ screens/ReservationScreen.js
@@ -3,7 +3,7 @@ import { Picker } from '@react-native-picker/picker';
 import DateTimePicker from '@react-native-community/datetimepicker'
 import * as Animatable from 'react-native-animatable'
 import { useState } from 'react';
-
+import * as Notifications from 'expo-notifications'
 
 
 
@@ -34,7 +34,14 @@ const ReservationScreen = () => {
                 },
                 {
                     text: 'OK',
-                    onPress: () => {resetForm()}
+
+                    onPress: () => {
+                        presentLocalNotification(
+                            date.toLocaleDateString('en-US')
+                        );
+                        resetForm();
+                    }
+
 
                 }
             ],
@@ -48,6 +55,34 @@ const ReservationScreen = () => {
         setHikeIn(false);
         setDate(new Date());
         setShowCalendar(false);
+    }
+
+    const presentLocalNotification = async (reservationDate) => {
+        const sendNotification =() => {
+            Notifications.setNotificationHandler({
+                handleNotification: async () => ({
+                    shouldShowAlert: true,
+                    shouldPlaySound: true,
+                    shouldSetBadge: true
+                })
+            })
+            Notifications.scheduleNotificationAsync({
+                content: {
+                    title: 'Your Campsite Reservation Search',
+                    body: `Search for ${reservationDate} requested`
+                },
+                trigger: null
+            })
+        }
+
+        let permissions = await Notifications.getPermissionsAsync()
+        if (!permissions.granted) {
+            permissions = await Notifications.requestPermissionsAsync();
+        }
+        if (permissions.granted) {
+            sendNotification();
+        }
+
     }
 
     return (
